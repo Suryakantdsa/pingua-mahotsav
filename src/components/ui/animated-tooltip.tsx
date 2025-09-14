479c7a8e3644aa36ec5781ad16951dc84c3dd914
Suryakant <dassuryakantadas@gmail.com>
2025-09-14 16:19:39 +0530

    image size decrease

--- src/components/ui/animated-tooltip.tsx
+++ src/components/ui/animated-tooltip.tsx
@@ -91,7 +91,7 @@ export const AnimatedTooltip = ({
             width={250}
             src={item.image}
             alt={item.name}
-            className="relative !m-0 h-18 w-18 rounded-full border-2 border-white object-cover object-top !p-0 transition duration-500 group-hover:z-30 group-hover:scale-105 "
+            className="relative !m-0 h-16 w-16 rounded-full border-2 border-white object-cover object-top !p-0 transition duration-500 group-hover:z-30 group-hover:scale-105 "
           />
         </div>
       ))}
